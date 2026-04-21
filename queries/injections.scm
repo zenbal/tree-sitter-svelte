77c81982e0c0581f0df7722e2334b9105bde3d5c
zenbal <80584946+zenbal@users.noreply.github.com>
2026-04-21 19:22:58 +0200

    fix: inject svelte_raw_text as TypeScript instead of JavaScript
    TypeScript is a syntactic superset of JavaScript, so all valid JS
    expressions parse identically as TS. This ensures type annotations
    in @attach, #snippet, and other Svelte rune expressions are
    highlighted correctly (e.g. HTMLElement, number).

--- queries/injections.scm
+++ queries/injections.scm
@@ -12,7 +12,7 @@
   (#set! injection.language "scss"))
 
 ((svelte_raw_text) @injection.content
-  (#set! injection.language "javascript"))
+  (#set! injection.language "typescript"))
 
 ((script_element
   (start_tag
